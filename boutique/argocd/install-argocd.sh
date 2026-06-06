81ce3acfac0285d936bba9e51ca8fe09a6cc57b5
Mehdi Jouhari <jouharimehdi5@gmail.com>
2026-06-06 18:01:45 +0200

    removze timeout from apply argo command

--- boutique/argocd/install-argocd.sh
+++ boutique/argocd/install-argocd.sh
@@ -8,7 +8,8 @@ kubectl apply -n argocd \
   -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.12.3/manifests/install.yaml
 
 echo "Waiting for ArgoCD server..."
-kubectl -n argocd rollout status deploy/argocd-server --timeout=180s
+kubectl -n argocd rollout status deploy/argocd-server \
+  #--timeout=180s
 
 echo "ArgoCD admin password:"
 kubectl -n argocd get secret argocd-initial-admin-secret \
